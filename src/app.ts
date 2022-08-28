1ece6cebf785ad69a3316e6cfa0afdd8a43b0b9d
Irfan Shah <irfns@protonmail.com>
2022-08-28 10:40:37 +0530

    trying some html to pdf

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import {Converter} from  'showdown';
+import * as Mustache from 'mustache';
+import wkhtmltopdf from "wkhtmltopdf";
+import * as fs from 'fs';
+
+type KeyedOptions<T,K extends keyof T,V> = {[k in K]: V}
+interface MarkdownPDFRenderParams {
+    templateName: string,
+    templateVariables: Map<string,string>,
+    theme: string
+}
+type MarkdownPDFRenderOptions = KeyedOptions<MarkdownPDFRenderParams, keyof MarkdownPDFRenderParams, any>
+
+function generatePDF(fileName: string, settings: MarkdownPDFRenderOptions) {
+    const mdtemplate = fs.readFileSync(`./templates/${settings.templateName}.md`, 'utf8');
+    const theme = fs.readFileSync(`./theme/${settings.theme}.html`, 'utf8');
+    const converter = new Converter();
+    const mdtext = Mustache.render(mdtemplate, settings.templateVariables);
+    const html = Mustache.render(theme, { body: converter.makeHtml(mdtext) });
+    wkhtmltopdf(html, { pageSize: 'A4' }).pipe(fs.createWriteStream(fileName));
+}
+
+const settings: MarkdownPDFRenderOptions = {
+    templateName: 'nda',
+    templateVariables: { company: 'Setu', tparty: 'Aagya' },
+    theme:  'slate'
+}
+
+generatePDF('out.pdf', settings);
