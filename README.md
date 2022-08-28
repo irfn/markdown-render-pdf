fe3eb26b0895a8d1bc312f96ea4429bfb6ce9a10
Irfan Shah <irfns@protonmail.com>
2022-08-28 11:23:41 +0530

    better theme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Prerequisites
+
+`brew install wkhtmltopdf`
+
+# Run Dev mode
+
+`yarn dev`
+
+## Theme
+
+In themes/ folder the $themeName.html has inline css which can be customised
--- src/app.ts
+++ src/app.ts
@@ -14,16 +14,17 @@ type MarkdownPDFRenderOptions = KeyedOptions<MarkdownPDFRenderParams, keyof Mark
 function generatePDF(fileName: string, settings: MarkdownPDFRenderOptions) {
     const mdtemplate = fs.readFileSync(`./templates/${settings.templateName}.md`, 'utf8');
     const theme = fs.readFileSync(`./theme/${settings.theme}.html`, 'utf8');
-    const converter = new Converter();
     const mdtext = Mustache.render(mdtemplate, settings.templateVariables);
+    const converter = new Converter({moreStyling: true});
     const html = Mustache.render(theme, { body: converter.makeHtml(mdtext) });
+    fs.writeFileSync("./out/render.html",html);
     wkhtmltopdf(html, { pageSize: 'A4' }).pipe(fs.createWriteStream(fileName));
 }
 
 const settings: MarkdownPDFRenderOptions = {
     templateName: 'nda',
     templateVariables: { company: 'Setu', tparty: 'Aagya' },
-    theme:  'slate'
+    theme:  'modest'
 }
 
 generatePDF('./out/out.pdf', settings);
